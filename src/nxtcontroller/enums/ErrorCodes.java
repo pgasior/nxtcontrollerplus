814cb1e35c6ce93e577639934f8a16de3301dce8
lukas.dilik@gmail.com <lukas.dilik@gmail.com@7af86717-8e38-a4f5-8ce5-c767c5b3722e>
2012-03-02 09:46:10 +0000

    vytvoreny bt manager

--- src/nxtcontroller/enums/ErrorCodes.java
+++ src/nxtcontroller/enums/ErrorCodes.java
@@ -2,6 +2,7 @@ package nxtcontroller.enums;
 
 public class ErrorCodes {
 	public static final int BLUETOOTH_NOT_FOUND = 0;
-	public static final int BLUETOOTH_NOT_ACTIVATED = 1;
+	public static final int BLUETOOTH_CANT_BE_ACTIVATED = 1;
 	public static final int CANT_CONNECT = 2;
+	public static final int BLUETOOTH_NOT_ACTIVATED = 3;
 }
