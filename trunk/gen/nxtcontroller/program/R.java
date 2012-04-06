0b374a22f6ed401065bc9ae711d96703dc224770
lukas.dilik@gmail.com <lukas.dilik@gmail.com@7af86717-8e38-a4f5-8ce5-c767c5b3722e>
2012-04-06 17:26:19 +0000

    refactoring vytvaranie bt prikaoz ako triedy kvazy framework na komunikaciu cez BT

--- trunk/gen/nxtcontroller/program/R.java
+++ trunk/gen/nxtcontroller/program/R.java
@@ -20,6 +20,7 @@ public final class R {
         public static final int bgColor=0x7f060002;
         public static final int deepBlue=0x7f060001;
         public static final int textBlack=0x7f060000;
+        public static final int white=0x7f060003;
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
@@ -27,23 +28,25 @@ public final class R {
         public static final int walle=0x7f020002;
     }
     public static final class id {
-        public static final int connectButton=0x7f070009;
-        public static final int controlModeLabel=0x7f070007;
-        public static final int controlPadView=0x7f07000a;
-        public static final int controls=0x7f070004;
-        public static final int controlsWrap=0x7f070003;
-        public static final int deviceName=0x7f070006;
-        public static final int disconnectButton=0x7f070008;
+        public static final int batteryLevelLable=0x7f070002;
+        public static final int connectButton=0x7f07000a;
+        public static final int controlModeLabel=0x7f070008;
+        public static final int controlPadView=0x7f07000b;
+        public static final int controls=0x7f070005;
+        public static final int controlsWrap=0x7f070004;
+        public static final int deviceName=0x7f070007;
+        public static final int disconnectButton=0x7f070009;
         public static final int leftPanel=0x7f070001;
         public static final int main=0x7f070000;
-        public static final int rightPanel=0x7f070002;
-        public static final int statusLabel=0x7f070005;
+        public static final int rightPanel=0x7f070003;
+        public static final int statusLabel=0x7f070006;
     }
     public static final class layout {
         public static final int main_screen=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int batteryLevel=0x7f04000b;
         public static final int closeLabel=0x7f040003;
         public static final int connectNXT=0x7f040001;
         public static final int disconnectNXT=0x7f040002;
